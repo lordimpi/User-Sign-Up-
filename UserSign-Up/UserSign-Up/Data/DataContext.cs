fa7b0f847946707c8f66c11f95afd2de8c512613
Santiago Acuña <snt-26@hotmail.com>
2022-11-20 15:19:27 -0500

    Add User table in first migration

---
+++ UserSign-Up/UserSign-Up/Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UserSign_Up.Models;
+
+namespace UserSign_Up.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<User> Users => Set<User>();
+    }
+}
